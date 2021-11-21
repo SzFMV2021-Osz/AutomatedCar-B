fb842443ca0dcfdd5ba657da7dd758107c95b7b6
Pintér Zoltán <zoltan.pinter@icellmobilsoft.hu>
2021-11-21 21:01:03 +0100

    Add Radar class

---
+++ src/AutomatedCar/SystemComponents/Sensors/Radar.cs
@@ -0,0 +1,27 @@
+﻿// <copyright file="Radar.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AutomatedCar.SystemComponents.Sensors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Radar : SystemComponent
+    {
+        readonly int distance
+
+        public Radar(VirtualFunctionBus virtualFunctionBus)
+            : base(virtualFunctionBus)
+        {
+        }
+
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
