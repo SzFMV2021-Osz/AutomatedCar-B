51fa5ea7c32821c72fa86ecc72cf004b07b3f614
Pintér Zoltán <zoltan.pinter@icellmobilsoft.hu>
2021-11-23 21:32:38 +0100

    Adding a System Component to the Radar Class

---
+++ src/AutomatedCar/SystemComponents/Sensors/Radar.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Radar.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace AutomatedCar.SystemComponents.Sensors
+{
+    using System;
+
+    /// <summary>
+    /// Radar class.
+    /// </summary>
+    public class Radar : SystemComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Radar"/> class.
+        /// </summary>
+        /// <param name="virtualFunctionBus"></param>
+        public Radar(VirtualFunctionBus virtualFunctionBus)
+            : base(virtualFunctionBus)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
