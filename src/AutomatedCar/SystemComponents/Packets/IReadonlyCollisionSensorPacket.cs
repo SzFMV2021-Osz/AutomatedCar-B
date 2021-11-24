f946fd05871474b2f9e9d2c0cc69ce0e86e7d269
Drótos Barnabás <drotos.barnabas97@gmail.com>
2021-11-24 23:07:24 +0100

    Add collision type to readonyl packet

--- src/AutomatedCar/SystemComponents/Packets/IReadonlyCollisionSensorPacket.cs
+++ src/AutomatedCar/SystemComponents/Packets/IReadonlyCollisionSensorPacket.cs
@@ -2,6 +2,6 @@ namespace AutomatedCar.SystemComponents.Packets
 {
     public interface IReadonlyCollisionSensorPacket
     {
-        
+        public CollisionType CollisionType { get; }
     }
 }
