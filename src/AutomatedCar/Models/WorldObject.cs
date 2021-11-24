10a135011b3947f57954243cd8247f98d1d7ac5a
Szilagyi_FI2W6F <szilagyi189@gmail.com>
2021-11-24 20:55:55 +0100

    Revert "collision detection"
    This reverts commit 18d6d4f890fc5fd55a7ba36fb953105ffb243d84.

--- src/AutomatedCar/Models/WorldObject.cs
+++ src/AutomatedCar/Models/WorldObject.cs
@@ -4,8 +4,6 @@ namespace AutomatedCar.Models
     using System.Drawing;
     using Avalonia.Media;
     using ReactiveUI;
-    using Point = Avalonia.Point;
-    using Polygon = Avalonia.Controls.Shapes.Polygon;
 
     public class WorldObject : ReactiveObject
     {
@@ -13,9 +11,6 @@ namespace AutomatedCar.Models
         private int y;
 
         private double rotation;
-
-        //Ádám
-        public List<List<Point>> BasePoints { get; private set; }
 
         public WorldObject(int x, int y, string filename, int zindex = 1, bool collideable = false, WorldObjectType worldObjectType = WorldObjectType.Other)
         {
@@ -25,10 +20,6 @@ namespace AutomatedCar.Models
             this.ZIndex = zindex;
             this.Collideable = collideable;
             this.WorldObjectType = worldObjectType;
-            
-            //Ádám
-            this.NetPolygons = this.GenerateNetPolygons(polyPoints);
-
         }
 
         public int ZIndex { get; set; }
@@ -39,38 +30,16 @@ namespace AutomatedCar.Models
             set => this.RaiseAndSetIfChanged(ref this.rotation, value % 360);
         }
 
-        //Ádám
-        public List<Polygon> Polygons { get; set; }
-        public List<LineString> NetPolygons { get; set; }
-
         public int X
         {
             get => this.x;
-            set
-            {
-                //Ádám
-                this.UpdatePolygons();
-
-                this.RaiseAndSetIfChanged(ref this.x, value);
-            }
+            set => this.RaiseAndSetIfChanged(ref this.x, value);
         }
 
         public int Y
         {
             get => this.y;
-            set
-            {
-                //Ádám
-                this.UpdatePolygons();
-
-                this.RaiseAndSetIfChanged(ref this.y, value);
-            }
-        }
-
-        //Ádám
-        private void UpdatePolygons()
-        {
-            this.NetPolygons = this.GenerateNetPolygons(this.BasePoints);
+            set => this.RaiseAndSetIfChanged(ref this.y, value);
         }
 
         public Point RotationPoint { get; set; }
@@ -84,31 +53,6 @@ namespace AutomatedCar.Models
         public string Filename { get; set; }
 
         public bool Collideable { get; set; }
-
-        //Ádám
-        public bool IsColliding { get; set; }
-        private List<Polygon> GeneratePolygons(List<List<Point>> polyPoints)
-        {
-            List<Polygon> objectPolygons = new List<Polygon>();
-            foreach (List<Point> points in polyPoints)
-            {
-                objectPolygons.Add(new Polygon() { Points = points });
-            }
-
-            return objectPolygons;
-        }
-        public List<LineString> GenerateNetPolygons(List<List<Point>> polyPoints)
-        {
-            polyPoints = RotatePoints(polyPoints);
-            List<LineString> objectLineStrings = new List<LineString>();
-            foreach (List<Point> points in polyPoints)
-            {
-                var coordinates = points.Select(point => new Coordinate(point.X + this.referenceOffsetX + this.X, point.Y + this.referenceOffsetY + this.Y)).ToArray();
-                objectLineStrings.Add(new LineString(coordinates));
-            }
-
-            return objectLineStrings;
-        }
 
         public WorldObjectType WorldObjectType { get; set; }
     }
