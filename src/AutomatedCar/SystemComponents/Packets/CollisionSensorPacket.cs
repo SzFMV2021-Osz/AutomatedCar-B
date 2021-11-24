01a6abd192bb6a30354ca25db97268fe8dfc0103
Drótos Barnabás <drotos.barnabas97@gmail.com>
2021-11-24 22:09:04 +0100

    Add collision sensor packets

---
+++ src/AutomatedCar/SystemComponents/Packets/CollisionSensorPacket.cs
@@ -0,0 +1,12 @@
+namespace AutomatedCar.SystemComponents.Packets
+{
+    using ReactiveUI;
+
+    /// <summary>
+    /// Stores the collision detection packet data.
+    /// </summary>
+    public class CollisionSensor : ReactiveObject, IReadonlyCollisionSensorPacket
+    {
+        
+    }
+} 
---
+++ src/AutomatedCar/SystemComponents/Packets/IReadonlyCollisionSensorPacket.cs
@@ -0,0 +1,7 @@
+namespace AutomatedCar.SystemComponents.Packets
+{
+    public interface IReadonlyCollisionSensorPacket
+    {
+        
+    }
+}
